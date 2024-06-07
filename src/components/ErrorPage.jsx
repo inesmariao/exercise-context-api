8b7384edbe8fec7aa173ace736e4c52576d7ce6f
inesmariao <inesmao.misena@gmail.com>
2024-06-06 19:50:17 -0500

    ok

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -3,16 +3,18 @@ import './../styles/error-page.css';
 
 export const ErrorPage = () => {
   return (
-    <section id="not-found">
-      <div id="title">Error404</div>
-      <div className="circles">
-        <p>404<br/>
-        <small><strong>Ocurrió un ERROR página no encontrada</strong></small>
-        </p>
-        <span className="circle big"></span>
-        <span className="circle med"></span>
-        <span className="circle small"></span>
-      </div>
-  </section>
+    <body className='custom-body'>
+      <section id="not-found">
+        <div id="title">Error404</div>
+        <div className="circles">
+          <p>404<br/>
+          <small><strong>Ocurrió un ERROR página no encontrada</strong></small>
+          </p>
+          <span className="circle big"></span>
+          <span className="circle med"></span>
+          <span className="circle small"></span>
+        </div>
+      </section>
+    </body>
   )
 }
