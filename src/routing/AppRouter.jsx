977fd3d7d924a864a48db5cb681a5154f87bd2f5
inesmariao <inesmao.misena@gmail.com>
2024-06-06 21:00:33 -0500

    styles css ok

--- src/routing/AppRouter.jsx
+++ src/routing/AppRouter.jsx
@@ -11,36 +11,43 @@ export const AppRouter = () => {
   return (
     <Router>
       {/* Menú de navegación */}
-      <nav>
-        <ul>
-          <li>
-            <NavLink to='/'>Inicio</NavLink>
-          </li>
-          <li>
-            <NavLink to='/articulos'>Articulos</NavLink>
-          </li>
-          <li>
-            <NavLink to='/acerca-de'>Acerca de</NavLink>
-          </li>
-          <li>
-            <NavLink to='/contacto'>Contacto</NavLink>
-          </li>
-          <li>
-            <NavLink to='/login'>Identifícate</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <header className='header-nav'>
+        <nav>
+          <div className='logo'>
+            <h2>LOGO</h2>
+          </div>
+          <ul>
+            <li>
+              <NavLink to='/'>Inicio</NavLink>
+            </li>
+            <li>
+              <NavLink to='/articulos'>Artículos</NavLink>
+            </li>
+            <li>
+              <NavLink to='/acerca-de'>Acerca de</NavLink>
+            </li>
+            <li>
+              <NavLink to='/contacto'>Contacto</NavLink>
+            </li>
+            <li>
+              <NavLink to='/login'>Identifícate</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-      {/* Configurar las rutas */}
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/inicio' element={<Home />}></Route>
-        <Route path='/articulos' element={<Articles />}></Route>
-        <Route path='/acerca-de' element={<About />}></Route>
-        <Route path='/contacto' element={<Contact />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='*' element={<ErrorPage />}></Route>
-      </Routes>
+      <section className='content'>
+        {/* Configurar las rutas */}
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/inicio' element={<Home />}></Route>
+          <Route path='/articulos' element={<Articles />}></Route>
+          <Route path='/acerca-de' element={<About />}></Route>
+          <Route path='/contacto' element={<Contact />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='*' element={<ErrorPage />}></Route>
+        </Routes>
+      </section>
     </Router>
   )
 }
