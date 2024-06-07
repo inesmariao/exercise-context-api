fe1a1244a1c31df9495556faefa4312449093f83
inesmariao <inesmao.misena@gmail.com>
2024-06-07 18:47:11 -0500

    consumer Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from '../context/Context'
 
 export const Home = () => {
+
+  const contextShare = useContext(Context);
+
   return (
     <div>
       <h1>Página de Inicio</h1>
-      <p>Hola Bienvenido a mi página de inicio</p>
+      <p>Hola Bienvenid@ <strong>{contextShare}</strong>, a mi tienda!!</p>
     </div>
   )
 }
