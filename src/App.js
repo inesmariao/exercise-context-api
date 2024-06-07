30fa48b00e7079a602716b311c0f0a0a1735dbbd
inesmariao <inesmao.misena@gmail.com>
2024-06-06 19:43:51 -0500

    page error ok

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { AppRouter } from './routing/AppRouter';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AppRouter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <div>
+      <h1>Inicio de Sesión</h1>
+      <p>Hola Bienvenido a mi página de inicio de sesión</p>
+  </div>
+  )
+}
